49757d24800cc6f2968dd260dcfc97cbf4f67764
Eric Ray <erichamion@gmail.com>
2016-07-16 12:11:29 -0500

    Addition/subtraction

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,72 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Write("Integer 1: ");
+            int a;
+            String aStr = Console.ReadLine();
+            bool isAValid = Int32.TryParse(aStr, out a);
+            //int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Operator: ");
+            String oper = Console.ReadLine();
+            Console.Write("Integer 2: ");
+            int b;
+            String bStr = Console.ReadLine();
+            bool isBValid = Int32.TryParse(bStr, out b);
+            //int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            bool success;
+            int result = 0;
+            if (isAValid && isBValid)
+            {
+                switch (oper)
+                {
+                    case "+":
+                        result = a + b;
+                        success = true;
+                        break;
+
+                    case "-":
+                        result = a - b;
+                        success = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("'{0}' is not a valid operator!", oper);
+                        success = false;
+                        break;
+                }
+            }
+            else
+            {
+                String invalidValue;
+                String invalidSide;
+                if (!isAValid)
+                {
+                    invalidValue = aStr;
+                    invalidSide = "Left";
+
+                }
+                else
+                {
+                    invalidValue = bStr;
+                    invalidSide = "Right";
+                }
+                Console.WriteLine("{0}-side value of '{1}' is not a valid integer!", invalidSide, invalidValue);
+                success = false;
+            }
+
+            if (success)
+            {
+                Console.WriteLine("{0} {1} {2} = {3}", a, oper, b, result);
+            }
+            else
+            {
+                Console.WriteLine("Cannot calculate result");
+            }
+
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadKey();
         }
     }
 }
