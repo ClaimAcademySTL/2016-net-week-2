34f2a9d176842b76c8e68bbd280011779b430249
Russell Brooks <Russellc.brooks@gmail.com>
2016-07-18 13:48:43 -0500

    update

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,92 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            /* 
+             Console.WriteLine("Enter first integer");
+             int a = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter second integer");
+             int b = Convert.ToInt32(Console.ReadLine());
+
+             Console.WriteLine("Enter operation + or -");
+             string operation = Console.ReadLine();
+
+             if (operation == "+")
+             Console.WriteLine("The sum of {0} and {1} is {2}",a,b, a + b);
+
+             else 
+             Console.WriteLine("The difference between {0} and {1} is {2}",a,b,a - b);
+             Console.ReadLine();
+             */
+
+            /*
+            Console.WriteLine("Enter first integer");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second integer");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter operation +, -, *");
+            string operation = Console.ReadLine();
+
+            if (operation == "+")
+            { Console.WriteLine("The sum of {0} and {1} is {2}", a, b, a + b); }
+
+            else if (operation == "-")
+            { Console.WriteLine("The difference between {0} and {1} is {2}", a, b, a - b); }
+
+            else if (operation == "*")
+            { Console.WriteLine("The product of {0} and {1} is {2}", a, b, a * b); }
+
+            Console.ReadLine();
+            */
+
+            /*
+            Console.WriteLine("Enter first number");
+            float a = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            float b = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter operation +, -, *");
+            string operation = Console.ReadLine();
+
+            if (operation == "+")
+            { Console.WriteLine("The sum of {0} and {1} is {2}", a, b, a + b); }
+
+            else if (operation == "-")
+            { Console.WriteLine("The difference between {0} and {1} is {2}", a, b, a - b); }
+
+            else if (operation == "*")
+            { Console.WriteLine("The product of {0} and {1} is {2}", a, b, a * b); }
+
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Enter first number");
+            float a = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            float b = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter operation +, -, *, /");
+            string operation = Console.ReadLine();
+
+            if (operation == "+")
+            { Console.WriteLine("The sum of {0} and {1} is {2}", a, b, a + b); }
+
+            else if (operation == "-")
+            { Console.WriteLine("The difference between {0} and {1} is {2}", a, b, a - b); }
+
+            else if (operation == "*")
+            { Console.WriteLine("The product of {0} and {1} is {2}", a, b, a * b); }
+
+            else if (operation == "/")
+            { Console.WriteLine("The quotient of {0} and {1} is {2}", a, b, a / b); }
+
+            Console.ReadLine();
+
         }
+
     }
 }
