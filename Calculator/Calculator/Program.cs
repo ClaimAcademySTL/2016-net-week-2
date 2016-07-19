5f6a3347186e9e5de5309940a1c9a0dd83c0dbb1
Tommy Janicik <tjanicik@gmail.com>
2016-07-19 15:38:14 -0500

    Completed calculator project

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,78 +10,188 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            bool run, f, s, checkOp;
-            string firstInt, secondInt, opChar, checkRun;
-            int fInt, sInt;
+            bool run, f, s, checkOp, bothNeg,bothPos,firstNeg,firstPos;
+            string firstInp, secondInp, opChar, checkRun;
+            double fNum, sNum, upperBound, lowerBound;
 
-            run = true;
-
-            while (run)
+            do
             {
-                Console.WriteLine("Please enter an integer value");
-                firstInt = Console.ReadLine();
-                f = int.TryParse(firstInt, out fInt);
+                //Collecting user input of two numbers and checking for input errors
+                Console.WriteLine("Please enter a number");
+                firstInp = Console.ReadLine();
+                f = double.TryParse(firstInp, out fNum);
                 while (!f)
                 {
-                    Console.WriteLine("Invalid entry, please enter valid integer.");
-                    firstInt = Console.ReadLine();
-                    f = int.TryParse(firstInt, out fInt);
+                    Console.WriteLine("Invalid entry, please enter a valid number.");
+                    firstInp = Console.ReadLine();
+                    f = double.TryParse(firstInp, out fNum);
                 }
-                Console.WriteLine("Please enter another integer value");
-                secondInt = Console.ReadLine();
-                s = int.TryParse(secondInt, out sInt);
+                Console.WriteLine("Please enter another number.");
+                secondInp = Console.ReadLine();
+                s = double.TryParse(secondInp, out sNum);
                 while (!s)
                 {
-                    Console.WriteLine("Invalid entry, please enter valid integer.");
-                    secondInt = Console.ReadLine();
-                    s = int.TryParse(secondInt, out sInt);
+                    Console.WriteLine("Invalid entry, please enter a valid number.");
+                    secondInp = Console.ReadLine();
+                    s = double.TryParse(secondInp, out sNum);
                 }
 
-                Console.WriteLine("Please enter the operation symbol you would like to use on the two integers(+, -, /, or *).");
+                //checking which number is positive and which is negative, assign values to bools
+                if (fNum > 0 && sNum > 0)
+                {
+                    bothPos = true;
+                    bothNeg = false;
+                    firstPos = false;
+                    firstNeg = false;
+                }
+                else if (fNum < 0 && sNum < 0)
+                {
+                    bothPos = false;
+                    bothNeg = true;
+                    firstPos = false;
+                    firstNeg = false;
+                }
+                else if(fNum > 0 && sNum < 0)
+                {
+                    bothPos = false;
+                    bothNeg = false;
+                    firstPos = true;
+                    firstNeg = false;
+                }
+                else
+                {
+                    bothPos = false;
+                    bothNeg = false;
+                    firstPos = false;
+                    firstNeg = true;
+                }
+
+                //collecting user input for operator
+                Console.WriteLine("Please enter the operation symbol you would like to use on the two numbers(+, -, /, or *).");
                 opChar = Console.ReadLine();
-                checkOp = false;
-                while (!checkOp)
+
+                //checking for input error, boundary error, or printing out expression based on provided operator
+                do
                 {
                     switch (opChar)
                     {
                         case "+":
-                            Console.WriteLine("{0} + {1} = {2}", fInt, sInt, fInt + sInt);
+                            upperBound = Double.MaxValue - fNum;
+                            lowerBound = Double.MinValue - fNum;
+                            if (bothPos && sNum > upperBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if (bothNeg && sNum < lowerBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0} + {1} = {2}", fNum, sNum, fNum + sNum);
+                            }
                             checkOp = true;
                             break;
                         case "-":
-                            Console.WriteLine("{0} - {1} = {2}", fInt, sInt, fInt - sInt);
+                            upperBound = fNum - Double.MaxValue;
+                            lowerBound = fNum - Double.MinValue;
+                            if(firstPos && sNum < upperBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if(firstNeg && sNum>lowerBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0} - {1} = {2}", fNum, sNum, fNum - sNum);
+                            }
                             checkOp = true;
                             break;
                         case "/":
-                            Console.WriteLine("{0} / {1} = {2}", fInt, sInt, fInt / sInt);
+                            upperBound = fNum / Double.MaxValue;
+                            lowerBound = fNum / Double.MinValue;
+                            if (sNum == 0)
+                            {
+                                Console.WriteLine("Well...you can't divide by 0.");
+                            }
+                            else if (bothPos && sNum < upperBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if (bothNeg && sNum > upperBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if (firstPos && -(sNum) > lowerBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if(firstNeg && -(sNum) < lowerBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0} / {1} = {2}", fNum, sNum, fNum / sNum);
+                            }
                             checkOp = true;
                             break;
                         case "*":
-                            Console.WriteLine("{0} * {1} = {2}", fInt, sInt, fInt * sInt);
+                            upperBound = Double.MaxValue / fNum;
+                            lowerBound = Double.MinValue / fNum;
+                            if(bothPos && sNum > upperBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if(bothNeg && sNum < upperBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if(firstPos && sNum < lowerBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else if(firstNeg && -(sNum) < lowerBound)
+                            {
+                                Console.WriteLine("Oops, you went out of bounds.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0} * {1} = {2}", fNum, sNum, fNum * sNum);
+                            }
                             checkOp = true;
                             break;
                         default:
                             Console.WriteLine("Invalid entry, please enter valid operator.");
+                            opChar = Console.ReadLine();
+                            checkOp = false;
                             break;
                     }
-                }
+                } while (!checkOp);
 
+                //Collecting user input to decide whether to exit or evaluate another expression, while checking for input error
                 Console.WriteLine("Would you like to run again? (yes or no)");
                 checkRun = Console.ReadLine().ToLower();
-                while(checkRun != "yes" && checkRun != "no")
+                while (checkRun != "yes" && checkRun != "no")
                 {
                     Console.WriteLine("Invalid entry, please enter yes or no.");
                     checkRun = Console.ReadLine().ToLower();
                 }
-                if(checkRun == "no")
+                if (checkRun == "no")
                 {
                     run = false;
                     Console.WriteLine("Press any key to exit.");
                     Console.ReadKey();
                 }
+                else
+                {
+                    run = true;
+                }
                 Console.WriteLine();
                 Console.WriteLine();
-            }
+            } while (run);
         }
     }
 }
